b5dd16d95452e582f4fb9520924a25793ce159f0
Alexander <nth.alexx@gmail.com>
2019-10-09 15:39:04 +0300

    новая картинка товара

--- js/main.js
+++ js/main.js
@@ -38,8 +38,10 @@ $(document).ready(function () {
                 let item = $(this._lastFocusedEl).parents('.catalog-item');
                 let title = item.find('.catalog-item__title').text();
                 let price = item.find('.product-price').html();
+                let src = item.find('.catalog-item__image img').attr('src');
                 $('.product-modal__header').text(title);
                 $('.product-modal__price').html(price);
+                $('.product-modal__image img').attr('src', src);
             }
         }
     });
